f8232526bf2260cdfc0440b45f5c6632030e4bb3
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2022-02-20 21:35:15 -0600

    changes

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import CatalogList from "../../app/components/catalog/CatalogList";
+import FormCatalog from "../../app/components/catalog/FormCatalog";
 import HeaderFooterWraper from "../../app/components/layout/InitWraper";
 
-
-
 const index = () => {
+  const [AddList, setAddList] = useState(false);
+
   return (
     <HeaderFooterWraper>
-      <CatalogList />
+      {AddList ? (
+        <FormCatalog stateNew={AddList} addNew={setAddList}/>
+      ) : (
+        <CatalogList stateNew={AddList} addNew={setAddList} />
+      )}
     </HeaderFooterWraper>
   );
 };
